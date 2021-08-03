cc725e96f17826a47e453115ec3eafe384d8e66c
hun756 <ekemenms@hotmail.com>
2021-08-03 21:16:12 +0300

    Create Line Number for Wasm Output

---
+++ cpp/cheerp/line_nuber_w.cpp
@@ -0,0 +1,48 @@
+/**
+ *  @file line_number_w.cpp
+ *  @author Mehmet Ekemen <ekemenms@gmail.com>, <ekemenms@hotmail.com>
+ *  @brief 
+ *  @version 0.1
+ *  @date 2021-08-03
+ *  
+ *  @copyright Copyright (c) 2021
+ *  
+ *  @link https://leaningtech.com/cheerp/
+**/
+
+#include <string>
+#include <cheerp/clientlib.h>
+#include <cheerp/client.h>
+
+/**
+ *  @brief 
+ *      Application Starting Point
+ */
+
+[[cheerp::genericjs]] void countLineNumber(
+    client::HTMLInputElement* inputElem, 
+    client::Element* resultElem ) {
+
+    inputElem->addEventListener(
+        "input",
+        cheerp::Callback([inputElem, resultElem]() -> void
+                        {
+                            client::String *text = inputElem->get_value();
+
+                            client::RegExp *regex = new client::RegExp("\r\n|\r|\n");
+
+                            auto lines = text->split(regex);
+
+                            int count = lines->get_length();
+
+                            std::string _resStr = "Lines of number is : " + std::to_string(count);
+                            resultElem->set_textContent(_resStr.c_str());
+                        }
+        )
+    );
+
+}
+
+void webMain()
+{
+}
